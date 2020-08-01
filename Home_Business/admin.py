08ca9b23d7fa82e9c11732a3f3cd7286d8d27f16
franzgabriel21 <franzgabriel21.gc@gmail.com>
2020-08-01 21:31:26 +0800

    FINAL COMMIT

--- Home_Business/admin.py
+++ Home_Business/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from.models import Post
+from.models import VerifiedPost
 from.models import Portfolio
 from .models import Bid
 from .models import Apply
 from .models import Accept
 from .models import Employ
+from.models import Feedback
 
+admin.site.register(VerifiedPost)
 admin.site.register(Post)
 admin.site.register(Portfolio)
 admin.site.register(Bid)
 admin.site.register(Apply)
 admin.site.register(Accept)
 admin.site.register(Employ)
+admin.site.register(Feedback)
 admin.site.site_header = 'LikHanap Admin'
--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from accounts.models import User
 
 
 class AccountAdmin(UserAdmin):
-	list_display = ('email','username', 'name', 'usertype', 'city', 'street', 'date_joined', 'last_login', 'is_admin','is_staff')
+	list_display = ('email','username', 'name', 'usertype', 'city', 'street', 'verified', 'date_joined', 'last_login', 'is_admin','is_staff')
 	search_fields = ('email','username',)
 	readonly_fields=('date_joined', 'last_login')
 
